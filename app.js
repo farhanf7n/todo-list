2bddd4b328d99b8652caef7185d5c295adab083a
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-04-20 15:48:07 +0500

    Appended todoDiv to todoList

--- app.js
+++ app.js
@@ -28,5 +28,6 @@ function addTodo(event) {
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("complete-btn");
     todoDiv.appendChild(trashButton);
-
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv); 
 }
