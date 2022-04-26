0e650ee17b627fe79900b6eb759fe2920cbdb1bd
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-04-26 15:03:08 +0500

    Added Check and Trash Button's functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Selectors
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo)
 
 // Functions
 function addTodo(event) {
@@ -46,10 +48,49 @@ function deleteCheck(e) {
             todo.remove();
         });
     }
-
+    // ADD TODO TO LOCALSTORAGE
+    saveLocalTodos(todoInput.value);
     // CHECK MARK
     if(item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
 } 
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex"; 
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none"
+                }
+                break;
+        };
+    });
+}
+
+function saveLocalTodos(todo) {
+    // CHECK
+    let todos;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
